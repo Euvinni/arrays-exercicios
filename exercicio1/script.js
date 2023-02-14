707a9bfaa99f02863ea1b2a8f8360895926907c1
Euvinni <marcosvdsc1212@gmail.com>
2023-02-14 18:54:12 -0300

    Adicionado exercicios de arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+const arrayDeNumeros = [11, 7, 40, 859]
+
+const arrayDeStrings = [`violão`, `guitarra`, `vinicius`]
+
+const arrayDeDiversos = [`Chris`, 37, true, `perfume`]
+
+console.log(arrayDeNumeros)
+console.log(arrayDeStrings)
+console.log(arrayDeDiversos)
+
+
+
